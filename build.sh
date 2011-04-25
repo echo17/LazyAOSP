518b5b619eb3787f534903c066027285813e0f77
Aaron Lesley <echoside@gmail.com>
2011-04-25 05:40:37 -0700

    (more) proper indentation

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ echo "${red} CyanogenMod version only works with HTC Devices      "
 echo " Have you already run sync.sh? (y/n)"${rst}
 read snc
 case $snc in
-	y) echo " Ok good you may continue";;
-	n) echo " Please run sync.sh" exit;;
+    y) echo " Ok good you may continue";;
+    n) echo " Please run sync.sh" exit;;
 esac
 echo " Which device do you want to build for?"
 echo " 1 - Ace"
@@ -46,43 +46,43 @@ read device
 echo "Build time depends on your computer"
 echo "Press enter to start compiling"
 read waka
-	case $device in
-	1) dev=ace;;
-	2) dev=blade;;
-	3) dev=bravo;;
-	4) dev=bravoc;;
-	5) dev=buzz;;
-	6) dev=click;;
-	7) dev=crespo;;
-	8) dev=desire;;
-	9) dev=dream-sapphire;;
-	10) dev=encore;;
-	11) dev=espresso;;
-	12) dev=generic;;
-	13) dev=glacier;;
-	14) dev=harmony;;
-	15) dev=hero;;
-	16) dev=heroc;;
-	17) dev=incredible;;
-	18) dev=legend;;
-	19) dev=leo;;
-	20) dev=liberty;;
-	21) dev=one;;
-	22) dev=passion;;
-	23) dev=sholes;;
-	24) dev=speedy;;
-	25) dev=supersonic;;
-	26) dev=vega;;
-	27) dev=vision;;
-	28) dev=z71;;
-	29) dev=zepplin;;
-	30) dev=zero;;
-	esac
-		. build/envsetup.sh
-		brunch $dev		
+case $device in
+    1) dev=ace;;
+    2) dev=blade;;
+    3) dev=bravo;;
+    4) dev=bravoc;;
+    5) dev=buzz;;
+    6) dev=click;;
+    7) dev=crespo;;
+    8) dev=desire;;
+    9) dev=dream-sapphire;;
+    10) dev=encore;;
+    11) dev=espresso;;
+    12) dev=generic;;
+    13) dev=glacier;;
+    14) dev=harmony;;
+    15) dev=hero;;
+    16) dev=heroc;;
+    17) dev=incredible;;
+    18) dev=legend;;
+    19) dev=leo;;
+    20) dev=liberty;;
+    21) dev=one;;
+    22) dev=passion;;
+    23) dev=sholes;;
+    24) dev=speedy;;
+    25) dev=supersonic;;
+    26) dev=vega;;
+    27) dev=vision;;
+    28) dev=z71;;
+    29) dev=zepplin;;
+    30) dev=zero;;
+esac
+. build/envsetup.sh
+brunch $dev             
 if [ -e out/target/product/${dev}/*.zip ]
-	then
-	echo "Congrats! You will find the rom in out/target/product/${dev}"
-	else
-	echo "Something went wrong. Try looking for errors and use your friendly google"
+then
+    echo "Congrats! You will find the rom in out/target/product/${dev}"
+else
+    echo "Something went wrong. Try looking for errors and use your friendly google"
 fi
--- sync.sh
+++ sync.sh
@@ -10,18 +10,18 @@ echo "** Which device do you want to build for?  **"
 echo "** Make sure this script is in the dir you **"
 echo "** want AOSP source to be in. Is it? (y/n) **"
 read srcdir
-	if [ $srcdir = y ]
-		then
-		echo "Good! Now we need to sync Salvage Mod's Source code."
-		echo "This will take an hour plus depending on your connection."
-		echo "You will be asked your name and email then it will start syncing."
-		echo "Press Enter to continue"
-		read waka
-			repo init -u git://github.com/CyanogenMod/android.git -b gingerbread
-			repo sync -j12
-			git clone https://github.com/koush/proprietary_vendor_htc.git -b gingerbread vendor/htc/
-			echo "You're done syncing!! Please run build.sh to begin compiling."
-		elif [ $srcdir = n ]
-			then
-			echo "Please move this script to the right directory and start over."
-	fi
+if [ $srcdir = y ]
+then
+    echo "Good! Now we need to sync Salvage Mod's Source code."
+    echo "This will take an hour plus depending on your connection."
+    echo "You will be asked your name and email then it will start syncing."
+    echo "Press Enter to continue"
+    read waka
+    repo init -u git://github.com/CyanogenMod/android.git -b gingerbread
+    repo sync -j12
+    git clone https://github.com/koush/proprietary_vendor_htc.git -b gingerbread vendor/htc/
+    echo "You're done syncing!! Please run build.sh to begin compiling."
+elif [ $srcdir = n ]
+then
+    echo "Please move this script to the right directory and start over."
+fi
